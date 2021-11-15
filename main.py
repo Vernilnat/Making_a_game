5958edd0a9cc3a1562a87a85566b18a3093ec5fa
Vernilnat <vernilnat@gmail.com>
2021-11-15 13:01:31 +0100

    update

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import pygame
 
 
 WIDTH, HEIGHT = 512, 512
-
+WHITE = (255, 255, 255)
 
 def main():
-    pygame.display.set_mode((WIDTH, HEIGHT))
+    while True:
+        window.fill(WHITE)
 
 
 if __name__ == "__main__":
-    main()
+    pygame.init()
+    window = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("2048 av Verner Lindskog")
+
