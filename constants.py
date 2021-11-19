18ffadbd15177c30c0f9729ae128903ee91c6b07
Vernilnat <vernilnat@gmail.com>
2021-11-19 09:30:54 +0100

    update

--- constants.py
+++ constants.py
@@ -1,5 +1,13 @@
+import self as self
+
 WIDTH, HEIGHT = 512, 512
 BG_COL = (128, 152, 183)
 BLUE = (0, 0, 128)
 BLACK = (0, 0, 0)
-FPS = 144
+GAMEOVER_COL = (125, 255, 0)
+my_font = "Calibri"
+FPS = 60
+
+
+class BlockCol:
+    __init__(self, col)
--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import pygame
+from pygame.locals import (K_w, K_a, K_s, K_d, KEYDOWN, QUIT)
 import random
 import constants
 
+# Definiera en lista som har koll på spel-positionen
 positions = {
     (1, 1): 0, (2, 1): 0, (3, 4): 0, (4, 4): 0,
     (1, 2): 0, (2, 2): 0, (3, 3): 0, (4, 3): 0,
     (1, 3): 0, (2, 3): 0, (3, 2): 0, (4, 2): 0,
     (1, 4): 0, (2, 4): 0, (3, 1): 0, (4, 1): 0
 }
-
-pygame.font.init()
-font = pygame.font.SysFont("Calibri", 42)
 
 
 def drawblock(x, y, num):
@@ -23,32 +22,55 @@ def drawblock(x, y, num):
     window.blit(text, text_rect)
 
 
+def game_over():
+    game_over_text = font.render("Game Over!", True, constants.GAMEOVER_COL)
+
+
 def newblock():
+    # Kollar lediga rutor
     options = []
     for i in positions.items():
         if 0 in i:
             options.append(i[0])
+    if not options:
+        game_over()
     coords = random.choice(options)
-    print(coords)
     num = random.choice((2, 4))
-
-
-newblock()
+    drawblock(*coords, num)
+    # Lägg till värdet i positions-listan
+    positions[coords] = num
 
 
 def main():
     while True:
         window.fill(constants.BG_COL)
-        drawblock(1, 1, 10)
+        for block in positions:
+            if positions[block] is not 0:
+                drawblock(*block, positions[block])
+
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == KEYDOWN:
+                if event.key == K_w:
+                    newblock()
+                elif event.key == K_a:
+                    newblock()
+                elif event.key == K_s:
+                    newblock()
+                elif event.key == K_d:
+                    newblock()
+                else:
+                    pass
+            elif event.type == QUIT:
+                pygame.quit()
                 quit()
+
         pygame.display.update()
         pygame.time.Clock().tick(constants.FPS)
 
 
 if __name__ == "__main__":
     pygame.init()
+    font = pygame.font.SysFont(constants.my_font, 42)
     window = pygame.display.set_mode((constants.WIDTH, constants.HEIGHT))
     pygame.display.set_caption("2048 av Verner Lindskog")
     icon = pygame.image.load("imgs/2048_logo.png")
